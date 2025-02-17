82ad2948fbf189d0c1c510024191736056548c08
Nick Moores <NickMoores@users.noreply.github.com>
2025-02-17 22:21:51 +0000

    Update entrypoint.sh
    Added aspecx ref

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -ex
 
-exec supervisord -c /app/supervisord.conf
+# Startup background services
+supervisord -c /app/supervisord.conf
+
+# Start AspecX
+exec /venv/bin/python /aspecx.py
