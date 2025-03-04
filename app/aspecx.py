0e72db20e3f9dd9c3cabba36c8c682ca0b3987b5
Nick Moores <MooresN@itn.co.uk>
2025-03-04 12:52:02 +0000

    Added more options to Chrome to startup in the correct location.

--- app/aspecx.py
+++ app/aspecx.py
@@ -5,7 +5,10 @@ import os
 
 options = Options()
 options.add_argument("--kiosk")
+options.add_argument("--start-maximized")
+options.add_argument("--start-fullscreen")
 options.add_argument("--window-size=1920,1080")
+options.add_argument("--window-position=0,0")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -16,7 +19,7 @@ driver = webdriver.Chrome(options=options)
 try:
     # Navigate to webpage
     driver.get(os.environ['URL'])
-
+    
     GracefulExit().wait()
 finally:
     driver.quit()
